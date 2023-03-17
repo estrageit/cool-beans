0b6183f67d8478ee63265986c2fe9b175db90212
estrageit <me@estrageit.net>
2023-03-17 13:13:52 -0300

    vulkan tutorial 1

---
+++ lve_window..c
@@ -0,0 +1,33 @@
+#include "lve_window.h"
+
+#include<string.h>
+
+void lvewin_init(lve_window* lvewin){
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    lvewin->m_window = glfwCreateWindow(
+        lvewin->m_width,
+        lvewin->m_height,
+        lvewin->m_name,
+        NULL,
+        NULL);
+}
+
+lve_window* lvewin_make(int w, int h, const char* name){
+    lve_window* r = malloc(sizeof(lve_window));
+    r->m_height = h;
+    r->m_width = w;
+    char* n = malloc((strlen(name) + 1) * sizeof(char));
+    strcpy(n, name);
+    r->m_name = n;
+    lvewin_init(r);
+    return r;
+}
+
+void lvewin_destroy(lve_window* lvewin){
+    glfwDestroyWindow(lvewin->m_window);
+    glfwTerminate();
+    free(lvewin->m_name);
+    free(lvewin);
+}
---
+++ lve_window.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include<GLFW/glfw3.h>
+
+typedef struct lve_window{
+    GLFWwindow* m_window;
+    int m_width;
+    int m_height;
+    char* m_name;
+} lve_window;
+
+lve_window* lvewin_make(int w, int h, const char* name);
+void lvewin_destroy(lve_window* lvewin);
